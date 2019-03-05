42c23dfcc6f797bae73534ed0dc2efa72659568e
chiendarrendor@yahoo.com <chiendarrendor@yahoo.com>
2019-03-04 23:45:57 -0800

    adding an optional string to a FlattenSolvableTuple to allow description of what the tuple is doing

--- common/grid/logic/flatten/FlattenSolvableTuple.java
+++ common/grid/logic/flatten/FlattenSolvableTuple.java
@@ -17,13 +17,14 @@ public class FlattenSolvableTuple<T>
         antimoves.add(antimove);
     }
 
-    public FlattenSolvableTuple()
-    {
+    public FlattenSolvableTuple(String s) { code = s; }
+    public FlattenSolvableTuple() { this(null); }
 
-    }
 
-    public FlattenSolvableTuple(T c1,Object m1, T c2, Object m2)
+    public FlattenSolvableTuple(T c1,Object m1, T c2, Object m2) { this(null,c1,m1,c2,m2); }
+    public FlattenSolvableTuple(String s, T c1,Object m1, T c2, Object m2)
     {
+        code = s;
         addTuple(c1,m2);
         addTuple(c2,m1);
     }
