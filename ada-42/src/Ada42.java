a2eef02a74980336c917a88392ea3f73bad9b1c9
chiendarrendor@yahoo.com <chiendarrendor@yahoo.com>
2021-05-02 17:30:02 -0700

    Improving Adalogical Aenigma Clue #42 clue calculator for Adalogical Addenda #28 (still no AI...partial pentomino match is tricky)

--- ada-42/src/Ada42.java
+++ ada-42/src/Ada42.java
@@ -2,250 +2,268 @@
 // 1) file -> New Module From Exisiting Sources, use the 'common' directory
 // 2) Project Structure -> Add Dependency to project on module 'common'
 
+import grid.file.GridFileReader;
+import grid.graph.GridGraph;
+import grid.lambda.CellLambda;
+import grid.lambda.XYLambda;
+import grid.letter.LetterRotate;
+import grid.puzzlebits.Direction;
+import grid.puzzlebits.PentominoData;
+import grid.spring.GridFrame;
+import grid.spring.GridPanel;
+
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.Vector;
 
-public class Ada42
-{
-    private static class DistrictInfo
-    {
-        char districtId;
-        Vector<Point> spaces = new Vector<Point>();
-        int minx;
-        int maxx;
-        int miny;
-        int maxy;
-        int numNums = 0;
-
-        int getXExtent() { return maxx - minx + 1; }
-        int getYExtent() { return maxy - miny + 1; }
-
-        public DistrictInfo(char districtId, int x,int y,boolean hasNum)
-        {
-            this.districtId = districtId;
-            minx = x;
-            maxx = x;
-            miny = y;
-            maxy = y;
-            spaces.add(new Point(x,y));
-            if (hasNum) ++numNums;
-        }
-
-        public void AddCell(int x,int y,boolean hasNum)
-        {
-            if (x < minx) minx = x;
-            if (x > maxx) maxx = x;
-            if (y < miny) miny = y;
-            if (y > maxy) maxy = y;
-            spaces.add(new Point(x,y));
-            if (hasNum) ++numNums;
-        }
-    }
-
-    private static class BoardState
-    {
-        Map<Character,DistrictInfo> districts = new HashMap<>();
-        GridFileReader gfr;
-
-        public BoardState(String filename)
-        {
-            gfr = new GridFileReader(filename);
-            for (int x = 0 ; x < gfr.getWidth() ; ++x)
-            {
-                for (int y = 0 ; y < gfr.getHeight() ; ++y)
-                {
-                    if (!HasDistrict(x,y)) continue;
-                    char district = GetDistrict(x,y);
-                    if (districts.containsKey(district))
-                    {
-                        districts.get(district).AddCell(x,y,HasNumber(x,y));
-                    }
-                    else
-                    {
-                        DistrictInfo di = new DistrictInfo(district,x,y,HasNumber(x,y));
-                        districts.put(district,di);
-                    }
-                }
-            }
-        }
-
-        public char GetLetter(int x,int y) { return gfr.getBlock("LETTERS")[x][y].charAt(0); }
-        public boolean HasLetter(int x,int y) { return Character.isAlphabetic(GetLetter(x,y)); }
-        public char GetNumber(int x,int y) { return gfr.getBlock("NUMBERS")[x][y].charAt(0); }
-        public boolean HasNumber(int x,int y) { return Character.isDigit(GetNumber(x,y)); }
-        public char GetDistrict(int x,int y) { return gfr.getBlock("AREAS")[x][y].charAt(0);}
-        public boolean HasDistrict(int x,int y) { return Character.isAlphabetic(GetDistrict(x,y)); }
-
-        public int getWidth() { return gfr.getWidth(); }
-        public int getHeight() { return gfr.getHeight(); }
-
-    }
-
-
-
-
-    public static class MyListener implements GridPanel.GridListener
-    {
+public class Ada42 {
+    private static class MyGridListener implements GridGraph.GridReference {
         private BoardState bs;
-        public MyListener(BoardState bs) { this.bs = bs;}
 
-        public int getNumXCells() { return bs.getWidth(); }
-        public int getNumYCells() { return bs.getHeight(); }
-        public boolean drawGridNumbers() { return true; }
-        public boolean drawGridLines() { return true; }
-        public boolean drawBoundary() { return true; }
+        public MyGridListener(BoardState bs) {
+            this.bs = bs;
+        }
 
-        public boolean drawCellContents(int cx,int cy, BufferedImage bi)
-        {
+        @Override public int getWidth() {
+            return bs.getWidth();
+        }
+
+        @Override public int getHeight() {
+            return bs.getHeight();
+        }
+
+        @Override public boolean isIncludedCell(int x, int y) {
+            return bs.HasDistrict(x, y);
+        }
+
+        private boolean sameRegion(int x, int y, Direction d) {
+            Point op = d.delta(x, y, 1);
+            return bs.GetDistrict(x, y) == bs.GetDistrict(op.x, op.y);
+        }
+
+        @Override public boolean edgeExitsEast(int x, int y) {
+            return sameRegion(x, y, Direction.EAST);
+        }
+
+        @Override public boolean edgeExitsSouth(int x, int y) {
+            return sameRegion(x, y, Direction.SOUTH);
+        }
+    }
+
+    private static class BoardState {
+        GridFileReader gfr;
+        GridGraph gg;
+        PentominoData pd;
+
+        public BoardState(String filename) {
+            gfr = new GridFileReader(filename);
+            gg = new GridGraph(new MyGridListener(this));
+            pd = new PentominoData();
+        }
+
+        public char GetLetter(int x, int y) {
+            return gfr.getBlock("LETTERS")[x][y].charAt(0);
+        }
+
+        public boolean HasLetter(int x, int y) {
+            return Character.isAlphabetic(GetLetter(x, y));
+        }
+
+        public char GetNumber(int x, int y) {
+            return gfr.getBlock("NUMBERS")[x][y].charAt(0);
+        }
+
+        public boolean HasNumber(int x, int y) {
+            return Character.isDigit(GetNumber(x, y));
+        }
+
+        public char GetDistrict(int x, int y) {
+            return gfr.getBlock("AREAS")[x][y].charAt(0);
+        }
+
+        public boolean HasDistrict(int x, int y) {
+            return Character.isAlphabetic(GetDistrict(x, y));
+        }
+
+        public void forEachCell(XYLambda xyl) {
+            CellLambda.forEachCell(getWidth(), getHeight(), xyl);
+        }
+
+        public int getWidth() {
+            return gfr.getWidth();
+        }
+
+        public int getHeight() {
+            return gfr.getHeight();
+        }
+
+        public char getDistrictType(int x, int y) {
+            return pd.getType(gg.connectedSetOf(new Point(x, y)));
+        }
+
+        public int getDistrictNumberCount(int x, int y) {
+            Set<Point> points = gg.connectedSetOf(new Point(x, y));
+            int result = 0;
+            for (Point p : points) {
+                if (HasNumber(p.x, p.y)) ++result;
+            }
+            return result;
+        }
+
+        public int getDistrictLetterCount(int x,int y) {
+            Set<Point> points = gg.connectedSetOf(new Point(x, y));
+            int result = 0;
+            for (Point p : points) {
+                if (HasLetter(p.x, p.y)) ++result;
+            }
+            return result;
+        }
+
+        public int getDistrictClueCount(int x,int y) {
+            if (!gfr.hasVar("CLUETYPE")) throw new RuntimeException("file needs CLUETYPE");
+            if (gfr.getVar("CLUETYPE").equals("AENIGMA")) return getDistrictNumberCount(x,y);
+            if (gfr.getVar("CLUETYPE").equals("ADDENDA")) return getDistrictLetterCount(x,y);
+            throw new RuntimeException("Unknown Clue Type!");
+        }
+
+
+
+
+        public boolean isActivePentominoTtype(char p) {
+            return gfr.getVar("PENTOMINOTYPES").indexOf(p) != -1;
+        }
+    }
+
+
+    public static class MyListener implements GridPanel.GridListener, GridPanel.EdgeListener {
+        private BoardState bs;
+        private String[] lines;
+
+        public MyListener(BoardState bs, String[] lines) {
+            this.bs = bs;
+            this.lines = lines;
+        }
+
+        public int getNumXCells() {
+            return bs.getWidth();
+        }
+
+        public int getNumYCells() {
+            return bs.getHeight();
+        }
+
+        public boolean drawGridNumbers() {
+            return true;
+        }
+
+        public boolean drawGridLines() {
+            return true;
+        }
+
+        public boolean drawBoundary() {
+            return true;
+        }
+
+        public String[] getAnswerLines() {
+            return lines;
+        }
+
+        public boolean drawCellContents(int cx, int cy, BufferedImage bi) {
             Graphics2D g = (Graphics2D) bi.getGraphics();
 
-            if (bs.HasNumber(cx,cy)) GridPanel.DrawStringInCell(bi,Color.black,"" + bs.GetNumber(cx,cy));
-            if (bs.HasLetter(cx,cy))
-            {
-                GridPanel.DrawStringUpperLeftCell(bi, Color.black, ""+bs.GetLetter(cx,cy));
-
-                char district = bs.GetDistrict(cx,cy);
-
-                DistrictInfo di = bs.districts.get(district);
-                boolean isBase = (di.getXExtent() == 2 && di.getYExtent() == 3) ||
-                        (di.getXExtent() == 3 && di.getYExtent() == 2);
+            if (bs.HasNumber(cx, cy)) GridPanel.DrawStringInCell(bi, Color.black, "" + bs.GetNumber(cx, cy));
+            if (bs.HasLetter(cx, cy)) {
+                GridPanel.DrawStringUpperLeftCell(bi, Color.black, "" + bs.GetLetter(cx, cy));
+                boolean isBase = bs.isActivePentominoTtype(bs.getDistrictType(cx, cy));
 
                 GridPanel.DrawStringInCell(bi,
                         isBase ? Color.red : Color.blue,
-                        "" + LetterRotate.Rotate(bs.GetLetter(cx, cy), di.numNums));
+                        "" + LetterRotate.Rotate(bs.GetLetter(cx, cy), bs.getDistrictClueCount(cx, cy)));
 
+            }
+
+            if (!bs.HasDistrict(cx,cy)) {
+                g.setColor(Color.DARK_GRAY);
+                g.fillRect(0,0,bi.getWidth(),bi.getHeight());
             }
 
             return true;
         }
 
-    }
-
-    public static class MyEdgeListener implements GridPanel.EdgeListener
-    {
-        BoardState bs;
-        public MyEdgeListener(BoardState bs) { this.bs = bs; }
-
-        public EdgeDescriptor onBoundary() { return new GridPanel.EdgeListener.EdgeDescriptor(Color.black,5);}
-
-        public EdgeDescriptor toEast(int x,int y)
-        {
-            return new EdgeDescriptor(Color.black, bs.GetDistrict(x,y) == bs.GetDistrict(x+1,y) ? 1 : 5);
+        public EdgeDescriptor onBoundary() {
+            return new GridPanel.EdgeListener.EdgeDescriptor(Color.black, 5);
         }
 
-        public EdgeDescriptor toSouth(int x,int y)
-        {
-            return new EdgeDescriptor(Color.black, bs.GetDistrict(x,y) == bs.GetDistrict(x,y+1) ? 1 : 5);
+        public EdgeDescriptor toEast(int x, int y) {
+            return new EdgeDescriptor(Color.black, bs.GetDistrict(x, y) == bs.GetDistrict(x + 1, y) ? 1 : 5);
+        }
+
+        public EdgeDescriptor toSouth(int x, int y) {
+            return new EdgeDescriptor(Color.black, bs.GetDistrict(x, y) == bs.GetDistrict(x, y + 1) ? 1 : 5);
         }
     }
 
 
-
-    public static void main(String[] args)
-    {
-        BoardState bs = new BoardState("ada42.txt");
-
-        for (char district : bs.districts.keySet())
-        {
-            DistrictInfo di = bs.districts.get(district);
-            System.out.println("ID: " + district + " num count: " + di.numNums + " x extent: " + di.getXExtent() + " y extent: " + di.getYExtent());
-            for (Point p : di.spaces)
-            {
-                System.out.print("\t" + p.toString());
-            }
-            System.out.println("");
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Bad Command Line");
+            System.exit(1);
         }
 
+        BoardState bs = new BoardState(args[0]);
+        String[] lines = new String[]{"Adalogical Aenigma", "#42 Solver"};
+
+        StringBuffer sb = new StringBuffer();
+        bs.forEachCell((x, y) -> {
+            if (!bs.HasDistrict(x, y)) return;
+            if (!bs.HasLetter(x, y)) return;
+            if (!bs.isActivePentominoTtype(bs.getDistrictType(x, y))) return;
+            sb.append(LetterRotate.Rotate(bs.GetLetter(x, y), bs.getDistrictClueCount(x, y)));
+        });
+
+        lines[0] = sb.toString();
+        lines[1] = bs.gfr.getVar("SOLUTION");
 
 
+        MyListener myl = new MyListener(bs, lines);
+        GridFrame gridFrame = new GridFrame("Adalogical Aenigma #42", 1300, 768, myl, myl);
 
-        for (int y = 0 ; y < bs.getHeight() ; ++y)
-        {
-            for (int x = 0 ; x < bs.getWidth() ; ++x)
-            {
-                if (!bs.HasLetter(x,y)) continue;
-                if (!bs.HasDistrict(x,y)) continue;
-                char district = bs.GetDistrict(x,y);
+        Map<Character,StringBuffer> sbmap = new HashMap<>();
+        PentominoData pd = bs.pd;
 
-                DistrictInfo di = bs.districts.get(district);
+        for(char c: pd.getNames()) {
+            sbmap.put(c,new StringBuffer());
+        }
 
-                if ((di.getXExtent() == 2 && di.getYExtent() == 3) ||
-                        (di.getXExtent() == 3 && di.getYExtent() == 2))
-                {
-                    System.out.print(LetterRotate.Rotate(bs.GetLetter(x,y),di.numNums));
+        bs.forEachCell((x,y)->{
+            if (!bs.HasDistrict(x, y)) return;
+            if (!bs.HasLetter(x, y)) return;
+            char ad = bs.getDistrictType(x,y);
+            sbmap.get(ad).append( LetterRotate.Rotate(bs.GetLetter(x,y),bs.getDistrictClueCount(x,y)) );
+
+        });
+
+        for (char c : pd.getNames()) {
+            if (bs.isActivePentominoTtype(c)) continue;
+            System.out.println(c + ": " + sbmap.get(c).toString());
+        }
+
+        if (bs.gfr.hasVar("SECRETCOUNT")) {
+            int secretcount = Integer.parseInt(bs.gfr.getVar("SECRETCOUNT"));
+            for (int sidx = 1; sidx <= secretcount; ++sidx) {
+                String sname = "SECRET" + sidx;
+                StringBuffer sbuf = new StringBuffer();
+                for (char c : bs.gfr.getVar(sname).toCharArray()) {
+                    sbuf.append(sbmap.get(c));
                 }
-            }
-        }
-        System.out.println("");
-
-        for (int y = 0 ; y < bs.getHeight() ; ++y)
-        {
-            for (int x = 0 ; x < bs.getWidth() ; ++x)
-            {
-                if (!bs.HasLetter(x,y)) continue;
-                if (!bs.HasDistrict(x,y)) continue;
-                char district = bs.GetDistrict(x,y);
-
-                DistrictInfo di = bs.districts.get(district);
-
-                if (!((di.getXExtent() == 2 && di.getYExtent() == 3) ||
-                        (di.getXExtent() == 3 && di.getYExtent() == 2)))
-                {
-                    System.out.print(LetterRotate.Rotate(bs.GetLetter(x,y),di.numNums));
-                }
-            }
-        }
-        System.out.println("");
-
-        Map<Character,StringBuffer> strings = new HashMap<>();
-
-        for (int x = 0 ; x < bs.getWidth() ; ++x)
-        {
-            for (int y = 0 ; y < bs.getHeight() ; ++y)
-            {
-                if (!bs.HasDistrict(x,y)) continue;
-                char distid = bs.GetDistrict(x,y);
-                char pentype = bs.gfr.getVar(""+distid).charAt(0);
-                if (!strings.containsKey(pentype))
-                {
-                    strings.put(pentype,new StringBuffer());
-                }
+                System.out.println(sname + ": " + sbuf.toString());
             }
         }
 
-        for (int y = 0 ; y < bs.getHeight() ; ++y)
-        {
-            for (int x = 0 ; x < bs.getWidth() ; ++x)
-            {
-                if (!bs.HasDistrict(x,y)) continue;
-                if (!bs.HasLetter(x,y)) continue;
 
-                char district = bs.GetDistrict(x,y);
-                char pentype = bs.gfr.getVar("" + district).charAt(0);
-                int numcount = bs.districts.get(district).numNums;
-
-                for(Map.Entry<Character,StringBuffer> ent: strings.entrySet())
-                {
-                    ent.getValue().append(ent.getKey().charValue() == pentype ?
-                            LetterRotate.Rotate(bs.GetLetter(x,y),numcount) : ' ');
-                }
-            }
-        }
-
-        for (Map.Entry<Character,StringBuffer> ent: strings.entrySet())
-        {
-            System.out.println(ent.getKey() + ": " + ent.getValue());
-        }
-
-
-
-
-
-
-        GridFrame gridFrame = new GridFrame("Adalogical Aenigma #42", 1300, 768,
-                new MyListener(bs),new MyEdgeListener(bs));
     }
+
 }
